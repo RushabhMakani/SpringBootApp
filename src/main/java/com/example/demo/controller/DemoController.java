88d90ca0ad88dd4c52a7d53e7ee8ce7fda4b0189
Rushabh Makani <mt_0236@outlook.com>
2023-03-18 19:59:41 +0530

    -Add time.

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -4,11 +4,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class DemoController {
 
     @GetMapping("ping")
     public ResponseEntity<String> getPing() {
         return ResponseEntity.ok("pong");
+    }
+
+    @GetMapping("time")
+    public ResponseEntity<LocalDateTime> getTime() {
+        return ResponseEntity.ok(LocalDateTime.now());
     }
 }
